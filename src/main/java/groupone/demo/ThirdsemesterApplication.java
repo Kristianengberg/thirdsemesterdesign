b741a444768e0f288478afd871e75ffe89881972
Kristian Engberg <krissoengberg@gmail.com>
2020-12-14 11:42:33 +0100

    added spring boot project

---
+++ src/main/java/groupone/demo/ThirdsemesterApplication.java
@@ -0,0 +1,13 @@
+package groupone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdsemesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdsemesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/groupone/demo/ThirdsemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package groupone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdsemesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
