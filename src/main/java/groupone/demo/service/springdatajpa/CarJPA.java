7357bfe243935a823966751a15ef99970a6c504e
Kristian Engberg <krissoengberg@gmail.com>
2020-12-14 13:29:05 +0100

    carJPA

--- src/main/java/groupone/demo/service/springdatajpa/CarJPA.java
+++ src/main/java/groupone/demo/service/springdatajpa/CarJPA.java
@@ -1,35 +1,52 @@
 package groupone.demo.service.springdatajpa;
 
 import groupone.demo.model.Car;
+import groupone.demo.repository.CarRepository;
 import groupone.demo.service.CarService;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 public class CarJPA implements CarService {
 
+    private final CarRepository carRepository;
+
+    public CarJPA(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+
     @Override
     public Set<Car> findAll() {
-        return null;
+
+        Set<Car> set = new HashSet<>();
+        carRepository.findAll().forEach(set::add);
+        return set;
+
     }
 
     @Override
     public Car save(Car object) {
-        return null;
+        return carRepository.save(object);
     }
 
     @Override
     public void delete(Car object) {
+
+        carRepository.delete(object);
 
     }
 
     @Override
     public void deleteById(Long aLong) {
 
+        carRepository.deleteById(aLong);
+
     }
 
     @Override
     public Optional<Car> findById(Long aLong) {
-        return Optional.empty();
+        return carRepository.findById(aLong);
     }
 }
