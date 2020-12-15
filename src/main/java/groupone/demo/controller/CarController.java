66fd5c69b2130b076a9d2d1795ce62aba67b3a67
ElmelundJC <Christianelmelund@hotmail.com>
2020-12-15 21:19:55 +0100

    Update CarController.java

--- src/main/java/groupone/demo/controller/CarController.java
+++ src/main/java/groupone/demo/controller/CarController.java
@@ -1,10 +1,17 @@
 package groupone.demo.controller;
 
+import groupone.demo.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CarController {
+
+    private CarService carService;
+
+    public CarController(CarService carService){
+        
+    }
 
     @GetMapping("/carpage")
     public String carPage(){
