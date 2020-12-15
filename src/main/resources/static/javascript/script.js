048fd83262d8dbe896d6c85e295aa79bc7afd33b
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2020-12-15 10:05:12 +0100

    Slideshow added to frontpage

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,9 +1,11 @@
-var slideIndex = 0;
+
+
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
