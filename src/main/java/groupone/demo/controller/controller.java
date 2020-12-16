61c2b26437e89537cfe8d2c7e84ea2a00e91f12d
PR31SL3R <jesperpreisler@hotmail.com>
2020-12-16 09:59:40 +0100

    aboutus.html and Contacts.html

--- src/main/java/groupone/demo/controller/controller.java
+++ src/main/java/groupone/demo/controller/controller.java
@@ -1,4 +1,28 @@
 package groupone.demo.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class controller {
+
+
+    @GetMapping("/contacts")
+    public String returnContacts(){
+        return "contact/contact";
+    }
+
+    @GetMapping("/aboutus")
+    public String returnAboutUS(){
+        return "aboutus/aboutus";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "contact/test";
+    }
+
+
+
 }
